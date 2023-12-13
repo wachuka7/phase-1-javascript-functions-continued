3763d9ca7f4d8b120467ee7ffe71b9f90b038df4
wachuka7 <rachaelwachuka7@gmail.com>
2023-12-14 00:27:36 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity= "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+
+}
+
+activity("bathe my dog ");
+
+function mondayWork(activity2 = "go to the office") {
+    return `This Monday, I will ${activity2}.`
+}
+
+activity2("work from home");
+function wrapAdjective (star) {
+    return function(Rachael = "special") {
+        return `You are ${star}${Rachael}${star}!`
+    }
+}
